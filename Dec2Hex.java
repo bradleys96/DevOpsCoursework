2ffe4a4d19e2b45f2d1efaa0b87acdbcc9579e74
bradleys96 <Bscamb300@caledonian.ac.uk>
2024-11-13 21:27:24 +0000

    Fix syntax errors

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
-//This is a test comment
-//test number 2
+// This is a test comment
+// Test number 2
 
-class Dec2Hex
-{
-public static int Arg1;
-    public static void main(String args[])    {
+class Dec2Hex {
+    public static int Arg1;
 
-	// Check if an argument was provided
+    public static void main(String args[]) {
+        // Check if an argument was provided
         if (args.length == 0) {
             System.out.println("Error: No input provided. Please enter an integer.");
             return; // Exit program if no input
@@ -21,17 +20,18 @@ public static int Arg1;
             String hexadecimal = "";
             System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        while(num != 0)
-        
-            int rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
+            // Convert decimal to hexadecimal
+            while (num != 0) {
+                int rem = num % 16;
+                hexadecimal = ch[rem] + hexadecimal;
+                num = num / 16;
+            }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-	
-	} catch (NumberFormatException e) {
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
+
+        } catch (NumberFormatException e) {
             System.out.println("Error: Invalid input. Please enter a valid integer.");
         }
     }
 }
+
