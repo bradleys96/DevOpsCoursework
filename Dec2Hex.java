c6bd9cf7bf01df6a828d02f76c5eb743f89cf613
bradleys96 <Bscamb300@caledonian.ac.uk>
2024-11-13 13:05:42 +0000

    Add error handling for non-integer input

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -13,15 +13,15 @@ public static int Arg1;
             return; // Exit program if no input
         }
 
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        try {
+            // Try to parse the argument as an integer
+            int num = Integer.parseInt(args[0]);
+            char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            String hexadecimal = "";
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
-        {
+        
             rem=num%16;
             hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
